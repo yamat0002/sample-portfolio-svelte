d2db78f7c400e94ff16ff73d6e592ff13a43d9c4
ayat <ayat02@media.mit.edu>
2026-03-13 01:20:55 -0400

    lab 5

---
+++ src/lib/ProjectNarrative.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Scrolly from "svelte-scrolly";
+  import projects from "$lib/projects.json";
+
+  let scrollyProgress = 0;
+
+  let sorted_projects = projects.sort((a, b) => a.year - b.year);
+
+  let progressPerProject = 100 / sorted_projects.length;
+
+  $: activeProjectIdx = Math.min(
+    sorted_projects.length - 1,
+    Math.floor(scrollyProgress / progressPerProject)
+  );
+</script>
+
+<div class="scrolly-wrapper">
+  <Scrolly bind:progress={scrollyProgress}>
+    {#each sorted_projects as project}
+      <section class="step">
+        <div class="step-content">
+          <h3>{project.title} ({project.year})</h3>
+          <p>{project.story}</p>
+        </div>
+      </section>
+    {/each}
+
+    <svelte:fragment slot="viz">
+      <div class="project-detail">
+        <h3>{sorted_projects[activeProjectIdx].year}</h3>
+        <img
+          src={sorted_projects[activeProjectIdx].image}
+          alt={sorted_projects[activeProjectIdx].title}
+          width="100%"
+        />
+      </div>
+    </svelte:fragment>
+  </Scrolly>
+</div>
+
+<style>
+  .scrolly-wrapper {
+    width: min(1000ch, 60vw);
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .step {
+    min-height: 80vh;
+    padding: 2rem;
+  }
+
+  .step-content {
+    border-left: 0.3em solid var(--color-accent);
+    padding: 1.5rem 2rem;
+  }
+
+  .project-detail {
+    padding: 2rem;
+    width: 100%;
+  }
+
+  .project-detail img {
+    width: 100%;
+  }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,31 +3,29 @@
   import Project from "$lib/Project.svelte";
   import ProjectNarrative from "$lib/ProjectNarrative.svelte";
 
+  let years = projects.map(proj => proj.year);
+  let range = Math.max(...years) - Math.min(...years);
 </script>
 
 <svelte:head>
   <title>Projects</title>
-  </svelte:head>
+</svelte:head>
 
+<h1>{projects.length} Projects over {range} Years</h1>
+<p>Scroll down to see a timeline of my projects and how they've contributed to my professional and personal life</p>
 
-    <h1>{projects.length} Projects</h1>
-<p>Scroll down to see my a timeline of my projects and how they've contributed to my professional and personal life</p>
-   <ProjectNarrative />
+<ProjectNarrative />
 
 <div class="projects">
-  
   {#each projects as p}
-  <Project data={p} />
-
+    <Project data={p} />
   {/each}
 </div>
 
-<p class="outro">Thanks for scrolling through my project story! Feel free to explore all of the projects at your leisure below.</p>
-
+<p class="outro">Thanks for scrolling through my project story!!!</p>
 
 <style>
-  .outro{
+  .outro {
     margin-bottom: 2rem;
   }
 </style>
-
