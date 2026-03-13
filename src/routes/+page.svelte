f4bac06df19380916d795c35dc6657e0c2a3c827
ayat <ayat02@media.mit.edu>
2026-03-13 01:03:44 -0400

    stopped at adding scrolly will continue later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,57 @@
 <script>
+  import { onMount } from "svelte";
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
   import reading from "$lib/reading.json";
   import ReadingItem from "$lib/ReadingItem.svelte";
+  let githubData = null;
+  let loading = true;
+  let error = null;
+  onMount(async () => {
+    try {
+      console.log("Page has been mounted!")
+      let response = await fetch("https://api.github.com/users/yamat0002");
+      console.log(response);
+      githubData = await response.json();
+      console.log(githubData);
+    } catch (err) {
+      error = err;
+    }
+    loading = false;
+  });
 </script>
 
-
-
 <h1>Ayat Abodayeh</h1>
-
 <img
   src="images/ayat.jpg"
   class="profile-photo"
   alt="A cartoonish photo of me but in a beanie and done in picrew"
 />
-
 <p>
   I am an MAS Master's student at LLK and I enjoy being part of creative
   learning spaces and working towards greater social justice ~~~
 </p>
+
+{#if loading}
+    <p>Loading...</p>
+{:else if error}
+    <p>Something went wrong: {error.message}</p>
+{:else}
+    <section class="github-stats">
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
+            <dt>Public Gists</dt>
+            <dd>{githubData.public_gists}</dd>
+        </dl>
+    </section>
+{/if}
+
 <h2>Featured Projects</h2>
 <div class="projects">
   {#each projects.slice(0, 3) as p}
@@ -32,3 +65,28 @@
     <ReadingItem data={r} />
   {/each}
 </div>
+
+<style>
+    .github-stats dl {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 0.5em;
+        text-align: center;
+        padding: 1em;
+        border-left: 0.3em solid var(--color-accent);
+    }
+
+    .github-stats dt {
+        grid-row: 1;
+        font-weight: bold;
+        color: gray;
+        font-size: 0.9em;
+    }
+
+    .github-stats dd {
+        grid-row: 2;
+        margin: 0;
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
+  import ProjectNarrative from "$lib/ProjectNarrative.svelte";
 
 </script>
 
@@ -10,11 +11,23 @@
 
 
     <h1>{projects.length} Projects</h1>
-   <div class="projects">
+<p>Scroll down to see my a timeline of my projects and how they've contributed to my professional and personal life</p>
+   <ProjectNarrative />
+
+<div class="projects">
+  
   {#each projects as p}
   <Project data={p} />
 
   {/each}
 </div>
 
+<p class="outro">Thanks for scrolling through my project story! Feel free to explore all of the projects at your leisure below.</p>
+
+
+<style>
+  .outro{
+    margin-bottom: 2rem;
+  }
+</style>
 
